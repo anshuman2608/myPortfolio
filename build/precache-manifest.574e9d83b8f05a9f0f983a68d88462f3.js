d6bf3f1ba80248f2da0a0ea30f96d0a25caf41f8
Anshuman Singh <111238118+anshuman2608@users.noreply.github.com>
2023-03-29 10:18:55 +0530

    adding build

---
+++ build/precache-manifest.574e9d83b8f05a9f0f983a68d88462f3.js
@@ -0,0 +1,170 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f0a3c5e36d25e65849e396279a05a2f",
+    "url": "/myPortfolio/index.html"
+  },
+  {
+    "revision": "0e6041eb606fe4837705",
+    "url": "/myPortfolio/static/css/2.8850a738.chunk.css"
+  },
+  {
+    "revision": "66d81ae4ef0e734564a3",
+    "url": "/myPortfolio/static/css/main.299da081.chunk.css"
+  },
+  {
+    "revision": "0e6041eb606fe4837705",
+    "url": "/myPortfolio/static/js/2.573ceefa.chunk.js"
+  },
+  {
+    "revision": "332bf1b0d236b7fc5455596e6356764c",
+    "url": "/myPortfolio/static/js/2.573ceefa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66d81ae4ef0e734564a3",
+    "url": "/myPortfolio/static/js/main.5370003f.chunk.js"
+  },
+  {
+    "revision": "0f713a185793586653f3",
+    "url": "/myPortfolio/static/js/runtime-main.d351083c.js"
+  },
+  {
+    "revision": "c2b0f7bac5a2687df1376a73ebc1a002",
+    "url": "/myPortfolio/static/media/1.c2b0f7ba.jpg"
+  },
+  {
+    "revision": "80179bdc21fa404d86d3d3f400f050d7",
+    "url": "/myPortfolio/static/media/2.80179bdc.jpg"
+  },
+  {
+    "revision": "a140429380bf3d358b5a3cbfcb35b5ff",
+    "url": "/myPortfolio/static/media/3.a1404293.jpg"
+  },
+  {
+    "revision": "03c4002f9076efbfa9c4cbc8f56cb364",
+    "url": "/myPortfolio/static/media/4.03c4002f.jpg"
+  },
+  {
+    "revision": "a976e7d3ae0233e3053dc8612ae5bbfd",
+    "url": "/myPortfolio/static/media/5.a976e7d3.jpg"
+  },
+  {
+    "revision": "5f1527f1b3ea91cce509919b0be7ab0e",
+    "url": "/myPortfolio/static/media/cart.5f1527f1.jpg"
+  },
+  {
+    "revision": "67af05e12311bac06327b43ca31e166b",
+    "url": "/myPortfolio/static/media/earth.67af05e1.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/myPortfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/myPortfolio/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/myPortfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/myPortfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/myPortfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "9a19cb37ac018cd4deceb4190152a2c1",
+    "url": "/myPortfolio/static/media/homePage.9a19cb37.jpg"
+  },
+  {
+    "revision": "d10f4bf68a27526ef83fd9dfdc75317d",
+    "url": "/myPortfolio/static/media/icons8-css3.d10f4bf6.svg"
+  },
+  {
+    "revision": "642525f695e382a55e5991af2e3e77e9",
+    "url": "/myPortfolio/static/media/icons8-github.642525f6.svg"
+  },
+  {
+    "revision": "aa5dd6d833f6c703cd5f5dd94b67ac4b",
+    "url": "/myPortfolio/static/media/icons8-heroku.aa5dd6d8.svg"
+  },
+  {
+    "revision": "a0657d1cac3b20f1e113738056db3c63",
+    "url": "/myPortfolio/static/media/icons8-html-5.a0657d1c.svg"
+  },
+  {
+    "revision": "7efb0618760a882db2932c197b377241",
+    "url": "/myPortfolio/static/media/icons8-javascript.7efb0618.svg"
+  },
+  {
+    "revision": "e7fb59768394c3d3f70fdbfdb7953c36",
+    "url": "/myPortfolio/static/media/icons8-node-js.e7fb5976.svg"
+  },
+  {
+    "revision": "36b7b85f31f6999f552a500b99c855a0",
+    "url": "/myPortfolio/static/media/icons8-react.36b7b85f.svg"
+  },
+  {
+    "revision": "43e595c98488c0c652369791e4f16170",
+    "url": "/myPortfolio/static/media/icons8-redux.43e595c9.svg"
+  },
+  {
+    "revision": "4af6741d92d8a26e9ca1347002f2424d",
+    "url": "/myPortfolio/static/media/img1.4af6741d.jpg"
+  },
+  {
+    "revision": "fd54d9fb7fd2e85c28f1d5c05c4352a4",
+    "url": "/myPortfolio/static/media/img2.fd54d9fb.jpg"
+  },
+  {
+    "revision": "9ce646673afb918f87afd0d568caa1fd",
+    "url": "/myPortfolio/static/media/img3.9ce64667.jpg"
+  },
+  {
+    "revision": "280dda233f23babb2f56c1e7381f2e20",
+    "url": "/myPortfolio/static/media/img4.280dda23.jpg"
+  },
+  {
+    "revision": "e904e6f926bb5b59888932c18cb3b76b",
+    "url": "/myPortfolio/static/media/img5.e904e6f9.jpg"
+  },
+  {
+    "revision": "db599654b8d954f0e72be60fb0f17595",
+    "url": "/myPortfolio/static/media/img6.db599654.jpg"
+  },
+  {
+    "revision": "05acfdb568b3df49ad31355b19495d4a",
+    "url": "/myPortfolio/static/media/ionicons.05acfdb5.woff"
+  },
+  {
+    "revision": "24712f6c47821394fba7942fbb52c3b2",
+    "url": "/myPortfolio/static/media/ionicons.24712f6c.ttf"
+  },
+  {
+    "revision": "2c2ae068be3b089e0a5b59abb1831550",
+    "url": "/myPortfolio/static/media/ionicons.2c2ae068.eot"
+  },
+  {
+    "revision": "621bd386841f74e0053cb8e67f8a0604",
+    "url": "/myPortfolio/static/media/ionicons.621bd386.svg"
+  },
+  {
+    "revision": "c949c76ab7e08168c7af7a1808705ead",
+    "url": "/myPortfolio/static/media/login.c949c76a.jpg"
+  },
+  {
+    "revision": "3ec3891b37f8b0765b554f10806f0b3f",
+    "url": "/myPortfolio/static/media/male1.3ec3891b.png"
+  },
+  {
+    "revision": "30a18547c13c8146c17a733b9460c8a8",
+    "url": "/myPortfolio/static/media/product.30a18547.jpg"
+  },
+  {
+    "revision": "b39e812d86d0139dc249b8b4992da7b0",
+    "url": "/myPortfolio/static/media/profile.b39e812d.png"
+  }
+]);
